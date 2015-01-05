7c3920962ba90fd8445986369b245930497049bc
Sanborn <sanbornh@rogers.com>
2015-01-05 14:56:42 -0500

    Moved ntg-plottable into standalone module

--- ntg-plottable.js
+++ ntg-plottable.js
@@ -1,60 +1,6 @@
-app = angular.module('myApp', []);
+var plottableModule = angular.module('ntgPlottable', []);
 
-app.controller('MainCtrl', function ($scope) {
-  var counter = 1;
-
-  $scope.data = (function makeData() {
-    var data = [];
-
-    for(i = 1; i <= 100; i++) {
-      var point = {};
-
-      point.purple = Math.floor(Math.random() * 50);
-      point.blue = Math.floor(Math.random() * 100);
-      // point.x = counter++;
-      // point.y = counter++;
-
-      data.push(point);
-    }
-
-    return data;
-
-  }())
-
-  $scope.shuffle = function () {
-    (function makeData() {
-      var data = [];
-
-      for(i = 1; i <= 100; i++) {
-        var point = {};
-
-        point.purple = Math.floor(Math.random() * 50);
-      point.blue = Math.floor(Math.random() * 100);
-
-        data.push(point);
-      }
-
-      $scope.data = data;
-      console.log($scope.data);
-    }())  
-  }
-
-  $scope.increase = function () {
-    for(var i = 0; i < 50; i++) {
-      $scope.data[i].x++;
-      $scope.data[i].y++;
-    }
-  }
-
-  $scope.decrease = function () {
-   for(var i = 0; i < 50; i++) {
-      $scope.data[i].x--;
-      $scope.data[i].y--;
-    }
-  }
-});
-
-app.directive('plottableScatter', function (){
+plottableModule.directive('plottableScatter', function (){
 
   /*
    * Return an accessor for a given property on some data point
